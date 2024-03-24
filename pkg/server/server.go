9be6cda8613e11c80e0f8cae431496066cef57c0
LeonidSarkisyan <TOPsarkisyanTOP@yandex.ru>
2024-03-24 17:26:23 +0300

    pre prod

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"crypto/tls"
 	"net/http"
 	"time"
 )
@@ -11,14 +12,24 @@ type Server struct {
 }
 
 func (s *Server) Run(port string, handler http.Handler) error {
+	tlsConfig := &tls.Config{}
+
+	cert, err := tls.LoadX509KeyPair("/etc/letsencrypt/live/xn----7sbq0ccdbck.xn--p1ai/fullchain.pem", "/etc/letsencrypt/live/xn----7sbq0ccdbck.xn--p1ai/privkey.pem")
+	if err != nil {
+		return err
+	}
+	tlsConfig.Certificates = []tls.Certificate{cert}
+
 	s.httpServer = &http.Server{
-		Addr:           ":" + port,
+		Addr:           ":443",
 		Handler:        handler,
 		MaxHeaderBytes: 1 << 20,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
+		TLSConfig:      tlsConfig,
 	}
-	return s.httpServer.ListenAndServe()
+
+	return s.httpServer.ListenAndServeTLS("", "")
 }
 
 func (s *Server) Shutdown(ctx context.Context) error {
