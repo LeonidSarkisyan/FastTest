5bb49ce2f1fdec7e32eff72299d06da50d480fe8
LeonidSarkisyan <TOPsarkisyanTOP@yandex.ru>
2024-03-27 09:20:14 +0300

    pre prod

--- static/js/one_test.js
+++ static/js/one_test.js
@@ -108,11 +108,10 @@ Spruce.store("methods", {
         let questionID = $store.data.questions[$store.data.currentIndex].id
         const response = await axios.post(QUESTION_WITH_ID_URL(questionID))
         console.log($store.data.questions[$store.data.currentIndex].answers)
-        $store.data.questions[$store.data.currentIndex].answers.push({
+        $store.data.questions[$store.data.currentIndex].answers = [...$store.data.questions[$store.data.currentIndex].answers, {
             id: response.data.id,
             text: ""
-        })
-        console.log($store.data.questions[$store.data.currentIndex].answers)
+        }]
     },
 
     async updateAnswer(answerIndex) {
